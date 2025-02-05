313af574b4200e5a4d10b84f12b8c040411b5baf
nicholasjenkins <jenkinsnjames97@gmail.com>
2025-02-04 21:54:00 -0500

    first draft

--- data.py
+++ data.py
@@ -1,1 +1,7 @@
-# File with constants
+Urban_ROUTES_URL = 'https://cnt-fc04e2d8-7eba-4a5b-ab76-3e171a000e66.containerhub.tripleten-services.com'
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1ST St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'stop at the juice bar, please'
